d18a331dd1a5935b39fd4a86e2dfea5600b2cd69
Katrina Kizenbaha <katrinak@scandiweb.com>
2017-09-15 19:49:39 +0300

    Move index.html content to index.php

--- index.php
+++ index.php
@@ -4,3 +4,22 @@ require_once ('Connector/Crud.php');
 
 $db = new Crud();
 $db->read();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/clndr.min.js"></script>
+    <link rel="stylesheet" href="css/calendar.css" />
+</head>
+<body>
+    <div class="title">
+        <h1>Pair-programming Planner</h1>
+    </div>
+    <div class="content">
+        <p>Content will go here</p>
+
+    </div>
+</body>
+</html>
